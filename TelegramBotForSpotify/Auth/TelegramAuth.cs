64efa12d83c4eff02be61e0fd09c7e45f180788e
Bohdan Harabadzhyu <bohdan_harabadzhyu@outlook.com>
2023-12-14 08:59:44 +0200

    add TelegramService

---
+++ TelegramBotForSpotify/Auth/TelegramAuth.cs
@@ -0,0 +1,16 @@
+﻿namespace TelegramBotForSpotify.Auth;
+
+public class TelegramAuth
+{
+    private string botToken;
+
+    public TelegramAuth(string botToken)
+    {
+        this.botToken = botToken;
+    }
+
+    public string GetBotToken()
+    {
+        return botToken;
+    }
+}
---
+++ TelegramBotForSpotify/Services/TelegramService.cs
@@ -0,0 +1,35 @@
+﻿using Telegram.Bot;
+
+namespace TelegramBotForSpotify.Services;
+
+public class TelegramService
+{
+    private static TelegramService instance = null;
+    private readonly ITelegramBotClient _botClient;
+
+    private TelegramService(string botToken)
+    {
+        _botClient = new TelegramBotClient(botToken);
+    }
+
+    public static TelegramService Instance(string botToken)
+    {
+        if (instance == null)
+        {
+            instance = new TelegramService(botToken);
+        }
+
+        return instance;
+    }
+
+    public async Task SendMessage(string chatId, string text)
+    {
+        await _botClient.SendTextMessageAsync(chatId, text);
+    }
+
+    public async Task<List<Telegram.Bot.Types.Update>> GetUpdates(int offset = 0)
+    {
+        var updates = await _botClient.GetUpdatesAsync(offset);
+        return updates.ToList();
+    }
+}
